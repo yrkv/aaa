32283634ecb60e2560800d4fb7a788210fd7bdf6
yekuz20 <yekuz20@icstudents.org>
2016-07-03 17:20:40 -0500

    started writing the rendering code

--- Javascript.js
+++ Javascript.js
@@ -1,2 +1,11 @@
-//a simple meaningless message
-var message = "Hello World";
+
+var spriteSheet = new Image();
+spriteSheet.src = "spriteSheet.png";
+
+
+$(document).ready(function() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    
+    
+});
