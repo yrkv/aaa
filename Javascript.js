9a0e27137d7c71a51c3ecfb008f63e775e5d23ac
yekuz20 <yekuz20@icstudents.org>
2016-07-05 16:45:17 -0500

    created game loop

--- Javascript.js
+++ Javascript.js
@@ -1,14 +1,54 @@
+var running = false;
+var totalTicks = 0;
 
 var spriteSheet = new Image();
 spriteSheet.src = "spriteSheet1.png";
 
+var canvas, context;
 
-$(document).ready(function() {
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
+setTimeout(function() {
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
     context.imageSmoothingEnabled = false;
-    setTimeout(function() {
-        context.drawImage(spriteSheet, 0, 0, 16, 16, 0, 0, 16, 16);
-    },10);
     
-});
+},1);
+
+function update() {
+    
+}
+
+function render() {
+    context.drawImage(spriteSheet, 0, 0, 16, 16, 0, 0, 16, 16);
+}
+
+function run() {
+    var lastTime = +(new Date);
+    var timer = +(new Date);
+    var ms = 1000 / 60;
+    var delta = 0;
+    var frames = 0;
+    var updates = 0;
+    if (running) {
+        var intervalId = setInterval(function() {
+            var now = +(new Date);
+            delta += (now - lastTime) / ms;
+            lastTime = now;
+            while (delta >= 1) {
+                update();
+                updates++;
+                totalTicks++;
+                delta--;
+            }
+            render();
+            frames++;
+
+            if (+(new Date) - timer > 1000) {
+                timer += 1000;
+                console.log("fps: " + frames + ", ups:" + updates);
+                frames = 0;
+                updates = 0;
+            }
+            if (!running) clearInterval(intervalId);
+        },1);
+    }
+}
