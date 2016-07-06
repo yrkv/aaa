7b86c03c779c28660e238c103f4f19327d722a8a
yekuz20 <yekuz20@icstudents.org>
2016-07-06 03:16:07 -0500

    added loading a map and started some rendering stuff

--- Javascript.js
+++ Javascript.js
@@ -1,5 +1,47 @@
+
 var running = false;
 var totalTicks = 0;
+
+function Tile(spriteSheet, x, y, collision) {
+    this.spriteSheet = spriteSheet;
+    this.x = x;
+    this.y = y;
+    this.collision = collision;
+}
+
+var mapImage = new Image();
+
+var game = {};
+game.map = [];
+
+game.tiles = [];
+game.tiles[0] = new Tile(spriteSheet, 0, 2, false);
+game.tiles[1] = new Tile(spriteSheet, 0, 1, false);
+
+
+function convertPixel(pixel) {
+    pixel = "" + pixel[0] + " " + pixel[1] + " " + pixel[2] + " " + pixel[3];
+    switch (pixel) {
+        case "80 92 143 255": return 1;
+        default: return 0;
+    }
+}
+
+function loadMap(src) {
+    mapImage.src = src;
+    setTimeout(function() {
+        var hiddenCanvas = document.createElement("canvas");
+        var hiddenContext = hiddenCanvas.getContext("2d");
+        hiddenContext.drawImage(mapImage, 0, 0);
+        for (var x = 0; x < mapImage.width; x++) {
+            game.map[x] = [];
+            for (var y = 0; y < mapImage.height; y++) {
+                var data = hiddenContext.getImageData(x,y,1,1).data;
+                game.map[x][y] = convertPixel(data);
+            }
+        }
+    },10)
+}
 
 var spriteSheet = new Image();
 spriteSheet.src = "spriteSheet1.png";
@@ -10,7 +52,7 @@ setTimeout(function() {
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
     context.imageSmoothingEnabled = false;
-    
+    running = true;
 },1);
 
 function update() {
@@ -18,7 +60,11 @@ function update() {
 }
 
 function render() {
-    context.drawImage(spriteSheet, 0, 0, 16, 16, 0, 0, 16, 16);
+    for (var x = 0; x < 16; x++) {
+        for (var y = 0; y < 16; y++) {
+            context.drawImage(spriteSheet, game.tiles[game.map[x][y]].x * 16, game.tiles[game.map[x][y]].y * 16, 16, 16, x*16, y*16, 16, 16);
+        }
+    }
 }
 
 function run() {
